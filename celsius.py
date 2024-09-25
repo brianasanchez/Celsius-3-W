3181df19c5fa1a32c45aebb1bc4f85c2fb9c86be
brianasanchez <a.23308051281213@cbtis128.edu.mx>
2024-09-24 20:03:37 -0600

    Add files via upload

---
+++ celsius.py
@@ -0,0 +1,12 @@
+print(" ") # print imprime un espacio
+
+print("Briana Sarahi Sanchez Perez 3-W")
+print(" ") # print imprime un espacio
+
+Fahrenheit = float(input("Introduce los grados : ")) # te pide que ingreses los grados fahrenheit
+
+Celsius = (5/9) * (Fahrenheit-32) # formula para convertir a grados celsius
+
+print("La temperatura es : ", Celsius) # imprime la temperatura en grados celsius
+
+print(" ") # print imprime un espacio
